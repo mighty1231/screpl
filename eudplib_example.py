4b46bb4d87aef1134a1aa725914fa50d063f2b40
Seonghoi Lee <mighty1231@gmail.com>
2018-12-31 13:59:52 +0900

    Example for eudplib

---
+++ eudplib_example.py
@@ -0,0 +1,37 @@
+from eudplib import *
+
+@EUDFunc
+def main():
+	from repl import (
+		REPL, EUDCommand, argEncNumber, argEncUnit, RegisterCommand
+	)
+	if EUDInfLoop()():
+		# Turbo
+		DoActions(SetDeaths(203151, SetTo, 1, 0))
+		REPL(superuser=P1).execute() # The position of this function does not matter
+
+
+		# SEE EFFECT
+		UNIT = EUDVariable(initval = EncodeUnit("Terran Marine"))
+		COUNT = EUDVariable(initval = 1)
+		DoActions([
+			CreateUnit(COUNT, UNIT, "Anywhere", P1),
+			KillUnit(UNIT, P1)
+		])
+
+		# Make function that modifies variables - UNIT and CNT
+		@EUDCommand([argEncUnit, argEncNumber])
+		def SeeEffect(u, n):
+			UNIT << u
+			COUNT << n
+
+		# Register command for REPL
+		RegisterCommand("effect", SeeEffect)
+
+		# run map trigger and end trigger end trigger loop
+		RunTrigTrigger()
+		EUDDoEvents()
+	EUDEndInfLoop()
+
+LoadMap("base.scx")
+SaveMap("seeeffect.scx", main)
