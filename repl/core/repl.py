ddaf5035c4c2e2523bd1e15797e3a8f96eb992fc
Seonghoi Lee <mighty1231@gmail.com>
2020-04-19 13:36:30 +0900

    Change position of newline in repl

--- repl/core/repl.py
+++ repl/core/repl.py
@@ -77,7 +77,7 @@ class REPL(Application):
             cur_pn << 0
         EUDEndIf()
 
-        writer.write_f("SC-REPL, type help() ( %D / %D )",
+        writer.write_f("SC-REPL, type help() ( %D / %D )\n",
             cur_pn,
             f_div(repl_index + (PAGE_NUMLINES//2-1),
                 PAGE_NUMLINES//2)[0]
@@ -100,8 +100,8 @@ class REPL(Application):
         if EUDInfLoop()():
             EUDBreakIf(cur >= until)
 
-            writer.write_f('\n\x1C>>> \x1D%E', inputepd)
-            writer.write_f('\n%C%E', repl_outputcolor, outputepd)
+            writer.write_f('\x1C>>> \x1D%E\n', inputepd)
+            writer.write_f('%C%E\n', repl_outputcolor, outputepd)
 
             DoActions([
                 cur.AddNumber(1),
