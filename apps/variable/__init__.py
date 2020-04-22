39c7c26cbb9a9cbc88295f7ea15d6ad76f4c14ea
Seonghoi Lee <mighty1231@gmail.com>
2020-04-22 20:51:31 +0900

    Parse unit ids from Deaths and SetDeaths

--- apps/variable/__init__.py
+++ apps/variable/__init__.py
@@ -5,9 +5,48 @@ from repl import REPL, getAppManager, AppCommand, EPDConstString
 
 # initialize variables
 manager = getAppManager()
+death_units = set()
 
 def getUsedDeathUnits():
-    trig = GetChkTokenized().getsection(b'TRIG')
+    orig_triggers = GetChkTokenized().getsection(b'TRIG')
+    assert len(orig_triggers) % 2400 == 0
+
+    offset = 0
+    while offset < len(orig_triggers):
+        trig = orig_triggers[offset:offset+2400]
+
+        # parse triggers
+        conditions = []
+        for i in range(16):
+            condition = trig[20*i:20*(i+1)]
+            conditions.append(condition)
+
+        # parse actions
+        actions = []
+        for i in range(64):
+            action = trig[16*20+32*i:16*20+32*(i+1)]
+            actions.append(action)
+
+        # Find Deaths and SetDeaths
+        for condition in conditions:
+            condtype = condition[15]
+            player = b2i4(condition, 4)
+            unitid = b2i2(condition, 12)
+
+            if condtype == 15 and player <= 26:
+                death_units.add(unitid)
+
+        for action in actions:
+            acttype = action[26]
+            player = b2i4(action, 16)
+            unitid = b2i2(action, 24)
+
+            if acttype == 45 and player <= 26:
+                death_units.add(unitid)
+
+        offset += 2400
+
+getUsedDeathUnits()
 
 # make commands
 from .varapp import VariableApp
@@ -16,4 +55,4 @@ from .varapp import VariableApp
 def startCommand(self):
     manager.startApplication(VariableApp)
 
-REPL.addCommand('variable', startCommand)
+REPL.addCommand('var', startCommand)
