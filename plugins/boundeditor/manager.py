b7827780c74de9ac513efc8db7a41f3d72496f73
Seonghoi Lee <mighty1231@gmail.com>
2020-05-02 11:58:28 +0900

    BoundEditor: now user can delete any selected unit

--- plugins/boundeditor/manager.py
+++ plugins/boundeditor/manager.py
@@ -6,6 +6,47 @@ from .option import OptionApp
 from .pattern import PatternApp
 from .testmode import TestPatternApp
 
+def deleteSelectedUnit():
+    # 0x6284B8  CurrentUnitSelection     CUNIT *CurrentUnitSelection[12]
+    # 0x6284E8  AllPlayerSelectionGroups CUNIT *AllPlayerSelectionGroups[8][12]
+
+    # use location 1
+    le = EPD(0x58DC60)
+    te = EPD(0x58DC60) + 1
+    re = EPD(0x58DC60) + 2
+    be = EPD(0x58DC60) + 3
+    lv, tv, rv, bv = [f_dwread_epd(ee) for ee in [le, te, re, be]]
+
+    i = EUDVariable()
+    i << 0
+    if EUDInfLoop()():
+        EUDBreakIf(i >= 12)
+
+        cu_epd = f_epdread_epd(EPD(0x6284B8))
+        EUDBreakIf(cu_epd == EPD(0))
+
+        player_id = f_bread_epd(cu_epd + (0x4C//4), 0)
+        unit_type = f_wread_epd(cu_epd + (0x64//4), 0)
+        pos_x = f_wread_epd(cu_epd + (0x28//4), 0)
+        pos_y = f_wread_epd(cu_epd + (0x28//4), 2)
+
+        DoActions([
+            SetMemoryEPD(le, SetTo, pos_x),
+            SetMemoryEPD(te, SetTo, pos_y),
+            SetMemoryEPD(re, SetTo, pos_x),
+            SetMemoryEPD(be, SetTo, pos_y),
+            RemoveUnitAt(All, unit_type, 1, player_id)
+        ])
+        i += 1
+    EUDEndInfLoop()
+
+
+    # restore location 1
+    for ee, vv in zip([le, te, re, be], [lv, tv, rv, bv]):
+        f_dwwrite_epd(ee, vv)
+
+
+
 class BoundManagerApp(Application):
     def loop(self):
         if EUDIf()(appManager.keyPress("ESC")):
@@ -17,6 +58,8 @@ class BoundManagerApp(Application):
             appManager.startApplication(PatternApp)
         if EUDElseIf()(appManager.keyPress("T", hold=["LCTRL"])):
             appManager.startApplication(TestPatternApp)
+        if EUDElseIf()(appManager.keyPress("delete")):
+            deleteSelectedUnit()
         EUDEndIf()
 
     def print(self, writer):
@@ -25,6 +68,7 @@ class BoundManagerApp(Application):
         writer.write_f("LCTRL + P: Pattern\n")
         writer.write_f("LCTRL + T: Test\n")
         writer.write_f("Chat maphack() to turn on maphack\n")
+        writer.write_f("Press DELETE key to remove selected units\n")
         writer.write(0)
 
     @AppCommand([])
