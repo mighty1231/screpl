3333871026033820346b8db29b7e8c90f839835d
Seonghoi Lee <mighty1231@gmail.com>
2020-05-03 06:01:16 +0900

    BoundEditor: manage turbo types during test

--- plugins/boundeditor/testmode.py
+++ plugins/boundeditor/testmode.py
@@ -31,6 +31,12 @@ next_timer = EUDVariable(0)
 last_tick = EUDVariable(0)
 cur_tick = EUDVariable()
 
+TURBOMODE_EUDTURBO = 0
+TURBOMODE_TURBO    = 1
+TURBOMODE_NOTURBO  = 2
+TURBOMODE_END      = 3
+v_turbomode = EUDVariable(TURBOMODE_NOTURBO)
+
 class TestPatternApp(Application):
     def onInit(self):
         timer << 0
@@ -39,7 +45,7 @@ class TestPatternApp(Application):
         cur_tick << f_getgametick()
 
     def loop(self):
-        global timer, pattern_id, next_timer
+        global timer, pattern_id, next_timer, v_turbomode
         if EUDIf()(appManager.keyPress('ESC')):
             appManager.requestDestruct()
             EUDReturn()
@@ -49,6 +55,12 @@ class TestPatternApp(Application):
                 pattern_id.SetNumber(0),
                 next_timer.SetNumber(0),
             ])
+        if EUDElseIf()(appManager.keyPress('T')):
+            v_turbomode += 1
+            Trigger(
+                conditions = [v_turbomode.Exactly(TURBOMODE_END)],
+                actions = [v_turbomode.SetNumber(0)]
+            )
         EUDEndIf()
 
         # Create Tester Unit for every death
@@ -80,15 +92,39 @@ class TestPatternApp(Application):
         last_tick << cur_tick
         cur_tick << f_getgametick()
 
+        if EUDIf()(v_turbomode == TURBOMODE_EUDTURBO):
+            DoActions([SetMemory(0x6509A0, SetTo, 0)])
+        if EUDElseIf()(v_turbomode == TURBOMODE_TURBO):
+            DoActions([SetMemory(0x6509A0, SetTo, 1)])
+        EUDEndIf()
+
         appManager.requestUpdate()
 
     def print(self, writer):
         writer.write_f("Bound Editor Test Mode\n")
         writer.write_f("Press 'R' to reset, press 'ESC' to end test\n")
         writer.write_f("Timer = %D\n\n", timer)
+
+        obs_trig_delay = cur_tick - last_tick
         writer.write_f("Observed trigger delay = %D " \
-                "(1:eudturbo, 2:turbo, 31:nothing)\n\n", cur_tick - last_tick)
-        writer.write_f("Start location: ")
+                "(1:eudturbo, 2:turbo, 31:noturbo)\n", obs_trig_delay)
+
+        writer.write_f("Set trigger delay mode: ", )
+        if EUDIf()(v_turbomode == TURBOMODE_EUDTURBO):
+            writer.write_f("eudturbo")
+            if EUDIfNot()(obs_trig_delay == 1):
+                writer.write_f(" \x06Mismatch on delay: \x03wait trigger on somewhere?")
+            EUDEndIf()
+        if EUDElseIf()(v_turbomode == TURBOMODE_TURBO):
+            writer.write_f("turbo")
+            if EUDIfNot()(obs_trig_delay == 2):
+                writer.write_f(" \x06Mismatch on delay: \x03eudturbo trigger on somewhere?")
+            EUDEndIf()
+        if EUDElseIf()(v_turbomode == TURBOMODE_NOTURBO):
+            writer.write_f("no turbo")
+        EUDEndIf()
+
+        writer.write_f("\n\n\x02Start location: ")
         writeLocation(g_start_location)
         writer.write(ord('\n'))
         writer.write(0)
