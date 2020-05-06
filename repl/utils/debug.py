993573150f636378e6d6cf1da11733c8075750d3
Seonghoi Lee <mighty1231@gmail.com>
2020-05-06 14:23:52 +0900

    AppManager: getSTRSectionSize

--- repl/utils/debug.py
+++ repl/utils/debug.py
@@ -1,14 +1,14 @@
 from eudplib import *
 
 def f_raiseError(txt):
+    f_setcurpl(f_getuserplayerid())
     DoActions([
-        SetCurrentPlayer(f_getuserplayerid()),
         DisplayText(txt),
         SetMemory(0, Add, 1),
     ])
 
 def f_raiseWarning(txt):
+    f_setcurpl(f_getuserplayerid())
     DoActions([
-        SetCurrentPlayer(f_getuserplayerid()),
         DisplayText(txt),
     ])
