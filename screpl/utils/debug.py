566104eb96060b3cc148049dfd73bc4bcccbea3a
Seonghoi Lee <mighty1231@gmail.com>
2020-05-26 12:53:40 +0900

    New feature - DisplayWriter

--- screpl/utils/debug.py
+++ screpl/utils/debug.py
@@ -1,16 +1,17 @@
 from eudplib import *
 
 from . import conststring as cs
+from . import byterw as rw
 
 _buf = Db(2048)
 _writer = None
 
 def _get_writer():
+    """REPLByteRW instance that used only on this module"""
     global _writer
-    from screpl.utils.byterw import REPLByteRW
 
     if _writer is None:
-        _writer = REPLByteRW()
+        _writer = rw.REPLByteRW()
     return _writer
 
 @EUDFunc
@@ -50,3 +51,25 @@ def f_printf(*args):
     _get_writer().write_f(*args)
     _get_writer().write(0)
     _print_buf()
+
+class DisplayWriter:
+    """Helps to display text, preserving bottom part of chat
+
+    Usage
+
+    .. code-block: python
+
+        with DisplayWriter() as writer:
+            writer.write_f("Hello\n")
+            writer.write_f("Line number 2\n")
+            writer.write(0)
+    """
+    def __enter__(self):
+        writer = _get_writer()
+        writer.seekepd(EPD(_buf))
+        self.txt_ptr = f_dwread_epd(EPD(0x640B58))
+        return writer
+
+    def __exit__(self, ex_type, ex_val, tb):
+        _print_buf()
+        SeqCompute([(EPD(0x640B58), SetTo, self.txt_ptr)])
