044a791617cb8db65c0c293c4a1943fdc00e0dba
Seonghoi Lee <mighty1231@gmail.com>
2020-06-04 22:59:19 +0900

    Trigger: data structures for logging, #8

--- screpl/plugins/trigger/__init__.py
+++ screpl/plugins/trigger/__init__.py
@@ -4,7 +4,10 @@ from screpl.apps.repl import REPL
 from screpl.apps.logger import Logger
 from screpl.core.appcommand import AppCommand
 from screpl.main import get_app_manager
+from screpl.utils.debug import f_raise_error
 from eudplib.core.mapdata.stringmap import strmap
+
+from .entry import MaximumCircularBuffer, ResultEntry
 
 from .intacttrigger import IntactTrigger
 
@@ -70,79 +73,80 @@ def logger_log_trigger(epd):
         EUDEndLoopN()
         logwriter.write(0)
 
-@EUDFunc
-def _disassemble_and_log_trigger(trig_id, trig_epd):
+@EUDTypedFunc([None, MaximumCircularBuffer(ResultEntry)])
+def _disassemble_and_log_trigger(trig_epd, entry_table):
     trig_conditions = [Forward() for _ in range(16)]
-    trig_on_true = [Forward() for _ in range(16)]
-    trig_on_false = [Forward() for _ in range(16)]
-    trig_next_cond = [Forward() for _ in range(16)]
-    trig_branch, trig_condition_fail, trig_action = [Forward() for _ in range(3)]
-    # condition_count = EUDVariable(0)
-    # condition_states = EUDArray(16)
+    trig_copy = Forward()
 
-    # fill 0
+    cond_count = EUDVariable(0)
+    cond_bools = EUDArray(16)
+    cond_values = EUDArray(16)
+
+    # initialize
     DoActions(
-        # [condition_count.SetNumber(0)]
-        # + [SetMemory(condition_states + i, SetTo, 0) for i in range(16)]
-        [SetMemory(trig_action + 8 + 16*20 + i, SetTo, 0) for i in range(0, 32*64, 4)]
+        [cond_count.SetNumber(0)]
+        + [SetMemory(cond_bools + i, SetTo, 0) for i in range(16)]
+        + [SetMemory(cond_values + i, SetTo, 0) for i in range(16)]
     )
 
     # disassemble trigger - conditions
     for i in range(16):
         f_repmovsd_epd(
-            EPD(trig_conditions[i] + 8),
+            EPD(trig_conditions[i]),
             trig_epd + (8 + i*20)//4,
             20//4
         )
 
-    # actions
-    f_repmovsd_epd(
-        EPD(trig_action + 8 + 16*20),
-        trig_epd + (8 + 16*20)//4,
-        (2408 - 8 - 16*20) // 4
-    )
-
-    DoActions(
-        [SetNextPtr(trig_conditions[i], trig_on_false[i]) for i in range(16)]
-        + [SetNextPtr(trig_branch, trig_action)]
-    )
+    # trigger
+    f_repmovsd_epd(EPD(trig_copy)+2, trig_epd+2, 2400//4)
 
     if EUDExecuteOnce()():
         logger_log_trigger(trig_epd)
-        logger_log_trigger(EPD(trig_conditions[0]))
-        logger_log_trigger(EPD(trig_conditions[1]))
-        logger_log_trigger(EPD(trig_action))
+        logger_log_trigger(EPD(trig_conditions[0]-8))
+        logger_log_trigger(EPD(trig_conditions[1]-8))
+        logger_log_trigger(EPD(trig_copy))
     EUDEndExecuteOnce()
 
-    writer = Logger.get_writer()
-    writer.write_f("trig[%D] ", trig_id)
     for i in range(16):
         # check condtype == 0
         EUDJumpIf(
-            MemoryXEPD(EPD(trig_conditions[i] + 8 + 0xC), Exactly, 0, 0xFF000000),
-            trig_branch
+            MemoryXEPD(EPD(trig_conditions[i] + 0xC), Exactly, 0, 0xFF000000),
+            trig_copy
         )
-        trig_conditions[i] << IntactTrigger(
-            conditions = [],
-            actions = [SetNextPtr(trig_conditions[i], trig_on_true[i])]
-        )
-        trig_on_false[i] << NextTrigger()
-        writer.write_f("{}:X ".format(i))
-        RawTrigger(
-            nextptr = trig_next_cond[i],
-            actions = SetNextPtr(trig_branch, trig_condition_fail)
-        )
+        if EUDIf()(trig_conditions[i] << Memory(0, Exactly, 0)):
+            cond_bools[i] = 1
+        if EUDElse()():
+            # evaluate exact value
+            condtype = f_bread_epd(EPD(trig_conditions[i] + 0xC), 3)
+            cond_values[i] = 1123 # @TODO
+        EUDEndIf()
+        cond_count += 1
 
-        trig_on_true[i] << NextTrigger()
-        writer.write_f("{}:O ".format(i))
+    # call original trigger
+    trig_copy << IntactTrigger()
 
-        trig_next_cond[i] << NextTrigger()
+    # update
+    trig_end = Forward()
+    if EUDIfNot()(entry_table.empty()):
+        last_entry = entry_table.last()
+        if EUDIf()(last_entry.update(cond_count,
+                                     EPD(cond_bools),
+                                     EPD(cond_values)) == 0):
+            EUDJump(trig_end)
+        EUDEndIf()
+    EUDEndIf()
 
-    trig_branch << RawTrigger()
-    trig_action << IntactTrigger()
-    trig_condition_fail << NextTrigger()
+    # add entry
+    new_entry = ResultEntry.cast(f_dwread_epd(
+        entry_table.push_and_get_reference()))
+    tick = f_getgametick()
+    new_entry.start_tick = tick
+    new_entry.end_tick = tick
+    new_entry.cond_count = cond_count
+    f_repmovsd_epd(new_entry.cond_bools_epd, EPD(cond_bools), cond_count)
+    f_repmovsd_epd(new_entry.cond_values_epd, EPD(cond_values), cond_count)
 
-    writer.write(0)
+    trig_end << NextTrigger()
 
 
 class TrigInliningManager:
@@ -151,31 +155,79 @@ class TrigInliningManager:
         self.triggers = []
         self.trig_count = 0
 
+        # called result tables
+        self.result_tables = []
+        self.force_players = [set(), set(), set(), set()]
+        for pid in range(8):
+            self.force_players[GetPlayerInfo(pid).force].add(pid)
+        EUDRegisterObjectToNamespace("screpl_log_trigger", self.log_trigger)
+
     def register_trigger(self, trigger):
         assert isinstance(trigger, bytes)
+
+        # it should not have any wait action
+        for i in range(64):
+            action = trigger[16*20+32*i:16*20+32*(i+1)]
+            acttype = action[26]
+            if acttype == 0:
+                break
+            if acttype == 4:
+                raise ValueError("Trigger with Wait() action cannot be inlined")
+
         trig_object = IntactTrigger(trigSection=trigger)
 
         # build inlining player code
+        effplayers = set() # 0 ~ 8
         player_code = 0
-        for i in range(27):
-            if trigger[320 + 2048 + 4 + i]:
-                player_code |= 1 << i
+        for pid in range(27):
+            if trigger[320 + 2048 + 4 + pid]:
+                player_code |= 1 << pid
+                if pid <= 8:
+                    effplayers.add(pid)
+                elif pid == EncodePlayer(AllPlayers):
+                    effplayers |= set(range(8))
+                elif pid in list(map(EncodePlayer,
+                                     [Force1, Force2, Force3, Force4])):
+                    effplayers |= self.force_players[pid-EncodePlayer(Force1)]
+                else:
+                    raise ValueError(
+                        "Unknown behavior on trigger with player %d" % pid)
 
+        # create result entries
+        result_entries = {}
+        for pid in effplayers:
+            result_entries[pid] = MaximumCircularBuffer(ResultEntry).construct_w_empty([
+                ResultEntry.construct() for _ in range(10)
+            ])
+
+        # construct inline trigger
         inline_trig = (bytes(20)
                        + i2b4(0x10978d4a)
                        + i2b4(player_code)
-                       + ('screpl_log_trigger(%d)'
+                       + ('screpl_log_trigger(%d, f_getcurpl())'
                           % self.trig_count).encode())
         inline_trig += bytes(2400-len(inline_trig))
 
         self.trig_count += 1
         self.triggers.append(trig_object)
+        self.result_tables.append(result_entries)
 
         return inline_trig
 
-    def log_trigger(self, trig_id):
+    def log_trigger(self, trig_id, player_id):
         trigger = self.triggers[trig_id]
-        _disassemble_and_log_trigger(trig_id, EPD(trigger))
+        result_table = self.result_tables[trig_id]
+        _br = EUDIf
+        result_entry = EUDVariable()
+        for ep, entry in result_table.items():
+            _br()(player_id.Exactly(ep))
+            result_entry << entry
+            _br = EUDElseIf
+        if EUDElse()():
+            f_raise_error("screpl-trigger trig id %D - player id %D unknown",
+                          trig_id, player_id)
+        EUDEndIf()
+        _disassemble_and_log_trigger(EPD(trigger), result_entry)
 
     def find_signature_and_update(self):
         """Find trigger with debugging signature
@@ -216,8 +268,6 @@ def plugin_setup():
 
     tsm = TrigInliningManager()
     tsm.find_signature_and_update()
-
-    EUDRegisterObjectToNamespace("screpl_log_trigger", tsm.log_trigger)
 
     # make commands
     from .manager import TriggerManagerApp
