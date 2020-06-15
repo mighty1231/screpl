e4219a6eb4bfa374094f9979ebd852f6f8cdcbd6
Seonghoi Lee <mighty1231@gmail.com>
2020-06-15 23:20:40 +0900

    let UnitManager create or delete units

--- screpl/plugins/unit/manager.py
+++ screpl/plugins/unit/manager.py
@@ -8,6 +8,10 @@ from . import app_manager
 _unitid = EUDVariable(0)
 _resultref_unitid_epd = EUDVariable(EPD(0))
 
+DISP_MAIN = 0
+DISP_MANUAL = 1
+v_display_mode = EUDVariable()
+
 '''
 unitid
 0-227 (normal units)
@@ -17,6 +21,21 @@ unitid
 231 (buildings)
 232 (factories)
 '''
+
+@EUDTypedFunc([TrgPlayer, None, None, None])
+def _create_unit(playerid, unitid, x, y):
+    epds = [EPD(0x58DC60)+t for t in range(4)]
+    orig_values = [f_dwread_epd(epd) for epd in epds]
+
+    f_dwwrite_epd(epds[0], x)
+    f_dwwrite_epd(epds[1], y)
+    f_dwwrite_epd(epds[2], x)
+    f_dwwrite_epd(epds[3], y)
+
+    DoActions(CreateUnit(1, unitid, 1, playerid))
+
+    for epd, value in zip(epds, orig_values):
+        f_dwwrite_epd(epd, value)
 
 class UnitManagerApp(Application):
     fields = [
@@ -36,6 +55,8 @@ class UnitManagerApp(Application):
         self.resultref_unitid_epd = _resultref_unitid_epd
 
         self.focus_unit_id(_unitid)
+
+        v_display_mode << DISP_MAIN
 
         # restore initializing arguments
         _resultref_unitid_epd << EPD(0)
@@ -65,45 +86,85 @@ class UnitManagerApp(Application):
     def loop(self):
         unitid = self.unitid
         hold_ctrl = app_manager.key_holdcounter("LCTRL")
+        mouse_pos = app_manager.get_mouse_position()
+        selunit_epd = f_epdread_epd(EPD(0x6284B8))
+
         if EUDIf()(app_manager.key_press("ESC")):
             app_manager.request_destruct()
             EUDReturn()
+        if EUDElseIf()(app_manager.key_down("F1")):
+            v_display_mode << DISP_MANUAL
+            app_manager.request_update()
+        if EUDElseIf()(app_manager.key_up("F1")):
+            v_display_mode << DISP_MAIN
+            app_manager.request_update()
         if EUDElseIf()(app_manager.key_press("F7", sync=[hold_ctrl])):
             self.focus_unit_id(unitid - EUDTernary(hold_ctrl)(8)(1))
         if EUDElseIf()(app_manager.key_press("F8", sync=[hold_ctrl])):
             self.focus_unit_id(unitid + EUDTernary(hold_ctrl)(8)(1))
+        if EUDElseIf()(app_manager.key_press("1", sync=mouse_pos)):
+            _create_unit(P1, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("2", sync=mouse_pos)):
+            _create_unit(P2, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("3", sync=mouse_pos)):
+            _create_unit(P3, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("4", sync=mouse_pos)):
+            _create_unit(P4, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("5", sync=mouse_pos)):
+            _create_unit(P5, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("6", sync=mouse_pos)):
+            _create_unit(P6, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("7", sync=mouse_pos)):
+            _create_unit(P7, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("8", sync=mouse_pos)):
+            _create_unit(P8, unitid, mouse_pos[0], mouse_pos[1])
+        if EUDElseIf()(app_manager.key_press("DELETE", sync=[selunit_epd])):
+            if EUDIfNot()(selunit_epd == EPD(0)):
+                DoActions(SetMemoryXEPD(selunit_epd + 0x4C//4,
+                                        SetTo, 0, 0xFF00))
+            EUDEndIf()
         EUDEndIf()
 
     def print(self, writer):
-        writer.write_f("\x16Unit Manager\n")
+        if EUDIf()(v_display_mode == DISP_MAIN):
+            writer.write_f("\x16Unit Manager. Press F1 to get manual\n")
 
-        target_unitid = self.unitid
+            target_unitid = self.unitid
 
-        cur, until = EUDCreateVariables(2)
-        if EUDIf()(target_unitid == 232):
-            cur << 232
-            until << 232 + 1
-        if EUDElse()():
-            quot, mod = f_div(target_unitid, 8)
-            cur << quot * 8
-            until << cur + 8
-        EUDEndIf()
-
-        # fill contents
-        written_point = Forward()
-        if EUDInfLoop()():
-            EUDBreakIf(cur >= until)
-
-            if EUDIf()(cur == target_unitid):
-                writer.write(0x11) # orange
+            cur, until = EUDCreateVariables(2)
+            if EUDIf()(target_unitid == 232):
+                cur << 232
+                until << 232 + 1
             if EUDElse()():
-                writer.write(0x02) # pale blue
+                quot, mod = f_div(target_unitid, 8)
+                cur << quot * 8
+                until << cur + 8
             EUDEndIf()
 
-            writer.write_f(" %D: ", cur)
-            writer.write_unit(cur)
-            writer.write(ord('\n'))
+            # fill contents
+            written_point = Forward()
+            if EUDInfLoop()():
+                EUDBreakIf(cur >= until)
 
-            DoActions(cur.AddNumber(1))
-        EUDEndInfLoop()
+                if EUDIf()(cur == target_unitid):
+                    writer.write(0x11) # orange
+                if EUDElse()():
+                    writer.write(0x02) # pale blue
+                EUDEndIf()
+
+                writer.write_f(" %D: ", cur)
+                writer.write_unit(cur)
+                writer.write(ord('\n'))
+
+                DoActions(cur.AddNumber(1))
+            EUDEndInfLoop()
+        if EUDElse()():
+            writer.write_f(
+                "\x16Unit Manager Manual\n"
+                "Press ESC to destroy the unit manager\n"
+                "Press F7/F8 to change focus\n"
+                "Press F7/F8 with holding CTRL key to move focus at page level\n"
+                "Press 1, 2, ..., 8 to create focused unit on mouse cursor\n"
+                "Press DELETE to kill selected unit\n")
+        EUDEndIf()
         writer.write(0)
