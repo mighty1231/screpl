d989611bd3bdc3eab62241abe828e5574bb7afea
Seonghoi Lee <mighty1231@gmail.com>
2020-06-17 09:09:29 +0900

    BoundEditor: improves UI of option app

--- screpl/plugins/boundeditor/option.py
+++ screpl/plugins/boundeditor/option.py
@@ -92,102 +92,100 @@ class OptionApp(Application):
                 conditions=focus.Exactly(FOCUS_END),
                 actions=focus.SetNumber(0)
             )
-        if EUDElseIf()(app_manager.key_press("insert")):
+        if EUDElseIf()(app_manager.key_press("E", hold=["LCTRL"])):
             if EUDIf()(focus.Exactly(FOCUS_EFFECTPLAYER)):
                 SelectorApp.set_content(tb.Player, g_effectplayer)
                 app_manager.start_application(SelectorApp)
             if EUDElseIf()(focus.Exactly(FOCUS_EFFECTUNIT1)):
-                UnitManagerApp.set_content(g_effectunit_1, EPD(g_effectunit_1.getValueAddr()))
+                UnitManagerApp.set_content(
+                    g_effectunit_1,
+                    EPD(g_effectunit_1.getValueAddr()))
                 app_manager.start_application(UnitManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_EFFECTUNIT2)):
-                UnitManagerApp.set_content(g_effectunit_2, EPD(g_effectunit_2.getValueAddr()))
+                UnitManagerApp.set_content(
+                    g_effectunit_2,
+                    EPD(g_effectunit_2.getValueAddr()))
                 app_manager.start_application(UnitManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_EFFECTUNIT3)):
-                UnitManagerApp.set_content(g_effectunit_3, EPD(g_effectunit_3.getValueAddr()))
+                UnitManagerApp.set_content(
+                    g_effectunit_3,
+                    EPD(g_effectunit_3.getValueAddr()))
                 app_manager.start_application(UnitManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_OBSTACLEUNIT)):
-                UnitManagerApp.set_content(g_obstacle_unit, EPD(g_obstacle_unit.getValueAddr()))
+                UnitManagerApp.set_content(
+                    g_obstacle_unit,
+                    EPD(g_obstacle_unit.getValueAddr()))
                 app_manager.start_application(UnitManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_RUNNER_FORCE)):
                 SelectorApp.set_content(tb.Player, g_runner_force)
                 app_manager.start_application(SelectorApp)
             if EUDElseIf()(focus.Exactly(FOCUS_RUNNER_UNIT)):
-                UnitManagerApp.set_content(g_runner_unit, EPD(g_runner_unit.getValueAddr()))
+                UnitManagerApp.set_content(
+                    g_runner_unit,
+                    EPD(g_runner_unit.getValueAddr()))
                 app_manager.start_application(UnitManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_START_LOCATION)):
-                LocationManagerApp.set_content(g_start_location, EPD(g_start_location.getValueAddr()))
+                LocationManagerApp.set_content(
+                    g_start_location,
+                    EPD(g_start_location.getValueAddr()))
                 app_manager.start_application(LocationManagerApp)
             if EUDElseIf()(focus.Exactly(FOCUS_OBSCREATE_PATTERN)):
                 DoActions(g_obstacle_createpattern.AddNumber(1))
                 Trigger(
-                    conditions=g_obstacle_createpattern.Exactly(OBSTACLE_CREATEPATTERN_END),
-                    actions=g_obstacle_createpattern.SetNumber(0)
-                )
+                    conditions=g_obstacle_createpattern.Exactly(
+                        OBSTACLE_CREATEPATTERN_END),
+                    actions=g_obstacle_createpattern.SetNumber(0))
             if EUDElseIf()(focus.Exactly(FOCUS_OBSDESTRUCT_PATTERN)):
                 DoActions(g_obstacle_destructpattern.AddNumber(1))
                 Trigger(
-                    conditions=g_obstacle_destructpattern.Exactly(OBSTACLE_DESTRUCTPATTERN_END),
-                    actions=g_obstacle_destructpattern.SetNumber(0)
-                )
+                    conditions=g_obstacle_destructpattern.Exactly(
+                        OBSTACLE_DESTRUCTPATTERN_END),
+                    actions=g_obstacle_destructpattern.SetNumber(0))
             EUDEndIf()
         EUDEndIf()
         app_manager.request_update()
 
     def print(self, writer):
-        def _emphasize(val):
-            if EUDIf()(focus.Exactly(val)):
-                writer.write(0x11)
-            if EUDElse()():
-                writer.write(0x2)
-            EUDEndIf()
+        writer.write_f(
+            "\x16Bound Editor Options - F7, F8 to navigate, CTRL+E to modify\n"
+            "Effect player: %C%:constant;\n"
+            "\x16Effect unit 1/2/3: %C%:unit;\x16 / %C%:unit;\x16 / %C%:unit;\n"
+            "\x16Obstacle unit: %C%:unit;\n"
+            "\x16Runner force: %C%:constant;\n"
+            "\x16Runner unit: %C%:unit;\n"
+            "\x16Start location: %C%:location;\n",
+            EUDTernary(focus==FOCUS_EFFECTPLAYER)(0x11)(0x16),
+            (EPD(tb.Player), g_effectplayer),
+            EUDTernary(focus==FOCUS_EFFECTUNIT1)(0x11)(0x16),
+            (g_effectunit_1, ),
+            EUDTernary(focus==FOCUS_EFFECTUNIT2)(0x11)(0x16),
+            (g_effectunit_2, ),
+            EUDTernary(focus==FOCUS_EFFECTUNIT3)(0x11)(0x16),
+            (g_effectunit_3, ),
+            EUDTernary(focus==FOCUS_OBSTACLEUNIT)(0x11)(0x16),
+            (g_obstacle_unit, ),
+            EUDTernary(focus==FOCUS_RUNNER_FORCE)(0x11)(0x16),
+            (EPD(tb.Player), g_runner_force),
+            EUDTernary(focus==FOCUS_RUNNER_UNIT)(0x11)(0x16),
+            (g_runner_unit, ),
+            EUDTernary(focus==FOCUS_START_LOCATION)(0x11)(0x16),
+            (g_start_location, ))
 
-        writer.write_f("Bound Editor Options - F7, F8 to navigate, INSERT to modify\n")
-        writer.write_f("Effect player: ")
-        _emphasize(FOCUS_EFFECTPLAYER)
-        write_player(writer, g_effectplayer)
-
-        writer.write_f("\x02\nEffect unit 1/2/3: ")
-        _emphasize(FOCUS_EFFECTUNIT1)
-        writer.write_unit(g_effectunit_1)
-        writer.write_f("\x02, ")
-        _emphasize(FOCUS_EFFECTUNIT2)
-        writer.write_unit(g_effectunit_2)
-        writer.write_f("\x02, ")
-        _emphasize(FOCUS_EFFECTUNIT3)
-        writer.write_unit(g_effectunit_3)
-
-        writer.write_f("\x02\nobstacle unit: ")
-        _emphasize(FOCUS_OBSTACLEUNIT)
-        writer.write_unit(g_obstacle_unit)
-
-        writer.write_f("\x02\nRunner Force: ")
-        _emphasize(FOCUS_RUNNER_FORCE)
-        write_player(writer, g_runner_force)
-
-        writer.write_f("\x02\nRunner unit: ")
-        _emphasize(FOCUS_RUNNER_UNIT)
-        writer.write_unit(g_runner_unit)
-
-        writer.write_f("\x02\nStart location: ")
-        _emphasize(FOCUS_START_LOCATION)
-        writer.write_location(g_start_location)
-
-        writer.write_f("\x02\nOn obstacle be created, runner be ")
-        _emphasize(FOCUS_OBSCREATE_PATTERN)
-        for v, s in [(OBSTACLE_CREATEPATTERN_KILL, "killed"),
-                (OBSTACLE_CREATEPATTERN_REMOVE, "removed"),
-                (OBSTACLE_CREATEPATTERN_ALIVE, "alive")]:
+        writer.write_f("\x16On obstacle be created, runner be ")
+        writer.write(EUDTernary(focus==FOCUS_OBSCREATE_PATTERN)(0x11)(0x16))
+        for v, s in [(OBSTACLE_CREATEPATTERN_KILL, "killed\n"),
+                     (OBSTACLE_CREATEPATTERN_REMOVE, "removed\n"),
+                     (OBSTACLE_CREATEPATTERN_ALIVE, "alive\n")]:
             if EUDIf()(g_obstacle_createpattern.Exactly(v)):
                 writer.write_f(s)
             EUDEndIf()
 
-        writer.write_f("\x02\nOn obstacle be destructed, obstacle be ")
-        _emphasize(FOCUS_OBSDESTRUCT_PATTERN)
-        for v, s in [(OBSTACLE_DESTRUCTPATTERN_KILL, "killed"),
-                (OBSTACLE_DESTRUCTPATTERN_REMOVE, "removed")]:
+        writer.write_f("\x16On obstacle be destructed, obstacle be ")
+        writer.write(EUDTernary(focus==FOCUS_OBSDESTRUCT_PATTERN)(0x11)(0x16))
+        for v, s in [(OBSTACLE_DESTRUCTPATTERN_KILL, "killed\n"),
+                     (OBSTACLE_DESTRUCTPATTERN_REMOVE, "removed\n")]:
             if EUDIf()(g_obstacle_destructpattern.Exactly(v)):
                 writer.write_f(s)
             EUDEndIf()
 
-        writer.write(ord('\n'))
         writer.write(0)
